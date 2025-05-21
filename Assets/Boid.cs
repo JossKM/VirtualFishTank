34edf85378aaed97f104d994189ae7295f170b46
JossKM <31990727+JossKM@users.noreply.github.com>
2025-05-21 11:24:44 -0400

    Lab 2

--- Assets/Boid.cs
+++ Assets/Boid.cs
@@ -2,33 +2,63 @@ using UnityEngine;
 
 public class Boid : MonoBehaviour
 {
-    private GameObject targetObject;
-    private Rigidbody rigidBody;
+    public Rigidbody rigidBody;
 
     public float speedMax = 2;
     public float accelMax = 3;
 
     private void Start()
     {
-        targetObject = GameObject.Find("target");
         rigidBody = GetComponent<Rigidbody>();
     }
 
-    private void FixedUpdate()
+    private void Update()
+    {
+        Debug.DrawRay(transform.position, rigidBody.linearVelocity, Color.red);
+    }
+
+    public Vector3 Seek(Vector3 target, float acceleration)
     {
         //Get displacement vector to target
-        Vector3 toTarget = targetObject.transform.position - transform.position;
+        Vector3 toTarget = target - transform.position;
 
-        //Normalize
+        //Normalize to get the direction to the target
         Vector3 toTargetNormalized = toTarget.normalized;
 
-        //Determine acceleration
-        Vector3 acceleration = toTargetNormalized * accelMax;
+        //Determine acceleration with a given magnitude
+        Vector3 accel = toTargetNormalized * acceleration;
 
-        rigidBody.linearVelocity += acceleration * Time.fixedDeltaTime;
+        return accel;
+    }
 
-        //Enforce a top speed
-        rigidBody.linearVelocity = Vector3.ClampMagnitude(rigidBody.linearVelocity, speedMax);
+    public Vector3 Pursue(Vector3 target, float acceleration, float desiredSpeed)
+    {
+        //Get displacement vector to target
+        Vector3 toTarget = target - transform.position;
+
+        //Normalize to get the direction to the target
+        Vector3 toTargetNormalized = toTarget.normalized;
+
+        //Determine Desired Velocity -- toward target!
+        Vector3 desiredVelocity = toTargetNormalized * desiredSpeed;
+
+        //Determine required change in velocity between current velocity and desired
+        Vector3 deltaVel = desiredVelocity - rigidBody.linearVelocity;
+
+        Vector3 accel = deltaVel.normalized * acceleration; // Accelerate in the direction of the required change in velocity
+        return accel;
+    }
+
+
+    private void FixedUpdate()
+    {
+        float speed = rigidBody.linearVelocity.magnitude;
+
+        if (speed > speedMax) 
+        {
+            //Enforce a top speed
+            rigidBody.linearVelocity = rigidBody.linearVelocity * speedMax / speed;
+        }
 
         //Orient toward velocity
         transform.forward = rigidBody.linearVelocity;
--- Assets/BoidSimulation.cs
+++ Assets/BoidSimulation.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class BoidSimulationControl : MonoBehaviour
 {
@@ -16,19 +17,29 @@ public class BoidSimulationControl : MonoBehaviour
     public ControlMode controlMode = ControlMode.Seek;
 
     public GameObject boidPrefab = null;
+    public GameObject foodPrefab = null;
+    public GameObject targetObject = null;
     public int numBoidsToSpawn = 10;
     public List<Boid> boids = null;
 
     private void Start()
     {
+        targetObject = GameObject.Find("target");
+
         //Spawn boids
-        for(int i = 0; i < numBoidsToSpawn; i++) 
+        for (int i = 0; i < numBoidsToSpawn; i++)
         {
             Vector3 position = new Vector3(Random.Range(-1.4f, 1.4f), Random.Range(0f, 1.4f), Random.Range(-0.9f, 0.9f));
             Quaternion rotation = Random.rotation;
 
-            GameObject spawnedBoid = Instantiate(boidPrefab, position, rotation);
-            boids.Add(spawnedBoid.GetComponent<Boid>());
+            GameObject spawnedBoid = Instantiate(boidPrefab, position, rotation); //Instantiate a copy of the prefab
+
+            Boid boidComponent = spawnedBoid.GetComponent<Boid>(); // Get boid component from the newly instantiated copy
+            //Randomize properties of each boid within a range!
+            boidComponent.speedMax = Random.Range(0.5f, 1.5f);
+            boidComponent.accelMax = Random.Range(0.5f, 1.5f);
+
+            boids.Add(boidComponent); // Add the new object to a list we can use later
 
             spawnedBoid.GetComponent<Renderer>().material.SetColor("_BaseColor", Random.ColorHSV(0, 1, 0f, 1f, 0.5f, 1f));
 
@@ -38,9 +49,10 @@ public class BoidSimulationControl : MonoBehaviour
         }
     }
 
+
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Alpha1))
+        if (Input.GetKeyDown(KeyCode.Alpha1))
         {
             controlMode = ControlMode.Seek;
         }
@@ -56,5 +68,110 @@ public class BoidSimulationControl : MonoBehaviour
         {
             controlMode = ControlMode.Obstacle;
         }
+
+        if(Input.GetMouseButtonDown(0) && controlMode == ControlMode.Food)
+        {
+            SpawnFood();
+        }
     }
+
+    private void FixedUpdate()
+    {
+        //Have all fish seek food
+        for (int i = 0; i < boids.Count; i++) // For each boid...
+        {
+            float foodSeekRadius = 0.5f;
+            Collider[] colliders = Physics.OverlapSphere(boids[i].transform.position, foodSeekRadius);
+            foreach(Collider collider in colliders) // for each collider in this radius...
+            {
+                Food food = collider.GetComponent<Food>(); // Check if it has a Food component
+                if (food != null) // If it has this component, it will not be null. If not null, we can seek the food.
+                {
+                    Vector3 accel = boids[i].Seek(collider.transform.position, boids[i].accelMax);
+                    boids[i].rigidBody.linearVelocity += accel * Time.fixedDeltaTime;
+                    Debug.DrawRay(boids[i].transform.position, accel, Color.green); // Draw acceleration
+                }
+            }
+        }
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitInfo; // Will store information about the intersection if any
+        bool didHit = Physics.Raycast(ray, out hitInfo, 100); // Raycast will return false if it misses, true if it hits,
+                                                              // while it will also fill in the passed parameter hitInfo
+                                                              // with useful information about what it hit
+
+        if (didHit)
+        {
+            targetObject.transform.position = hitInfo.point; // Teleport an object to the point of intersection from the raycast
+        }
+
+        switch (controlMode)
+        {
+            case ControlMode.Seek:
+                {
+                    SeekModeControl();
+                    break;
+                }
+
+            case ControlMode.Pursue:
+                {
+                    PursueModeControl();
+                    break;
+                }
+        }
+
+
+    }
+
+    private void SpawnFood()
+    {
+        Instantiate(foodPrefab, targetObject.transform.position, Random.rotation); //Instantiate a copy of a prefab for food
+    }
+
+    private void SeekModeControl()
+    {
+        for (int i = 0; i < boids.Count; i++) // For each boid...
+        {
+            //Call the Seek function on each boid to calculate an acceleration vector
+            Vector3 accel = boids[i].Seek(targetObject.transform.position, boids[i].accelMax);
+
+            if (Input.GetMouseButton(0))
+            {
+                boids[i].rigidBody.linearVelocity += accel * Time.fixedDeltaTime; // Apply acceleration
+                Debug.DrawRay(boids[i].transform.position, accel, Color.green); // Draw acceleration
+            }
+            else if (Input.GetMouseButton(1))
+            {
+                boids[i].rigidBody.linearVelocity -= accel * Time.fixedDeltaTime; // Apply acceleration
+                Debug.DrawRay(boids[i].transform.position, accel, Color.green); // Draw acceleration
+            }
+        }
+    }
+
+
+    private void PursueModeControl()
+    {
+        for (int i = 0; i < boids.Count; i++) // For each boid...
+        {
+            //Call the Pursue function on each boid to calculate an acceleration vector
+            Vector3 accel = boids[i].Pursue(targetObject.transform.position, boids[i].accelMax, boids[i].speedMax);
+
+            if (Input.GetMouseButton(0))
+            {
+                boids[i].rigidBody.linearVelocity += accel * Time.fixedDeltaTime; // Apply acceleration
+                Debug.DrawRay(boids[i].transform.position, accel, Color.green); // Draw acceleration
+            }
+            else if (Input.GetMouseButton(1))
+            {
+                boids[i].rigidBody.linearVelocity -= accel * Time.fixedDeltaTime; // Apply acceleration
+                Debug.DrawRay(boids[i].transform.position, accel, Color.green); // Draw acceleration
+            }
+
+           
+            boids[i].rigidBody.linearVelocity += accel * Time.fixedDeltaTime; // Apply acceleration
+            Debug.DrawRay(boids[i].transform.position, accel, Color.green); // Draw acceleration
+        }
+    }
+
+
 }
