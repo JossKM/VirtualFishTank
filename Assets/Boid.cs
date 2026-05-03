8057a5ea7e70efe6faacb3fa95932057f7ff6000
JossKM <jossmooyoung@gmail.com>
2026-05-03 19:58:41 -0400

    Removing behaviours to make starter

--- Assets/Boid.cs
+++ Assets/Boid.cs
@@ -19,175 +19,40 @@ public class Boid : MonoBehaviour
         Debug.DrawRay(transform.position, rigidBody.linearVelocity, Color.red);
     }
 
-    //Seek a random point ahead in a sphere ahead of the boid
-    public Vector3 Wander(float distance, float radius, float acceleration)
-    {
-        Vector3 sphereCenter = transform.position + transform.forward * distance;
-        Vector3 randomPoint = sphereCenter + (radius * Random.insideUnitSphere);
-
-        return Seek(randomPoint, acceleration);
-    }
-
-    public Vector3 Seek(Vector3 target, float acceleration)
-    {
-        //Get displacement vector to target
-        Vector3 toTarget = target - transform.position;
-
-        //Normalize to get the direction to the target
-        Vector3 toTargetNormalized = toTarget.normalized;
-
-        //Determine acceleration with a given magnitude
-        Vector3 accel = toTargetNormalized * acceleration;
-
-        return accel;
-    }
-
-    public Vector3 Pursue(Vector3 target, float acceleration, float desiredSpeed)
-    {
-        //Get displacement vector to target
-        Vector3 toTarget = target - transform.position;
-
-        //Normalize to get the direction to the target
-        Vector3 toTargetNormalized = toTarget.normalized;
-
-        //Determine Desired Velocity -- toward target!
-        Vector3 desiredVelocity = toTargetNormalized * desiredSpeed;
-
-        //Determine required change in velocity between current velocity and desired
-        Vector3 deltaVel = desiredVelocity - rigidBody.linearVelocity;
-
-        Vector3 accel = deltaVel.normalized * acceleration; // Accelerate in the direction of the required change in velocity
-        return accel;
-    }
-
-    public Vector3 Arrive(Vector3 target, float acceleration, float arriveInnerRadius, float arriveOuterRadius)
-    {
-        //Get displacement vector to target
-        Vector3 toTarget = target - transform.position;
-
-        //Get distance to target
-        float distance = toTarget.magnitude;
-
-        //Get direction to target
-        Vector3 toTargetNormalized = toTarget.normalized; // careful not to divide by zero...
-
-        //Get desired velocity
-        Vector3 desiredVelocity = toTargetNormalized * speedMax;
-
-        float distancePercentage = 1;
-
-        //scale desired velocity based on distance percentage
-        if (distance < arriveInnerRadius)
-        {
-            desiredVelocity = Vector3.zero;
-            DebugDrawing.DrawCircle(transform.position, Quaternion.Euler(90, 0, 0), arriveInnerRadius, 8, Color.red, Time.fixedDeltaTime);
-        } else if(distance < arriveOuterRadius)
-        {
-            //express distance as a percentage of the way to the outer radius...
-            // example:
-            // arriveOuterRadius = 10
-            // distance = 8
-            // distancePercentage = 8/10 or 80 percent
-            distancePercentage = distance / arriveOuterRadius; //
-            desiredVelocity *= distancePercentage;
-            DebugDrawing.DrawCircleDotted(transform.position, Quaternion.Euler(90,0,0), arriveOuterRadius, 16, 0.01f, 0.01f, Color.red, Time.fixedDeltaTime);
-        }
-
-        Vector3 deltaVel = desiredVelocity - rigidBody.linearVelocity;
-
-        Vector3 accel = deltaVel.normalized * acceleration * distancePercentage; // Accelerate in the direction of the required change in velocity
-        return accel;
-    }
-
-    public Vector3 ObstacleAvoidance(float lookaheadDistance, float acceleration)
-    {
-        Vector3 accelOut = Vector3.zero;
-
-        //Whiskers... left and right
-        Ray whiskerLeft = new Ray(transform.position,  Quaternion.AngleAxis(-20, transform.up) * transform.forward);
-        Ray whiskerRight = new Ray(transform.position, Quaternion.AngleAxis(20, transform.up) * transform.forward);
-
-        //Whiskers... above and below
-        Ray whiskerUp = new Ray(transform.position, Quaternion.AngleAxis(-20, transform.right) * transform.forward);
-        Ray whiskerDown = new Ray(transform.position, Quaternion.AngleAxis(20, transform.right) * transform.forward);
-
-        RaycastHit hitInfoLeft;
-        RaycastHit hitInfoRight;
-        RaycastHit hitInfoUp;
-        RaycastHit hitInfoDown;
-
-        bool didHitLeft = Physics.Raycast(whiskerLeft, out hitInfoLeft, lookaheadDistance);
-        if (didHitLeft)
-        {
-            //Turn right
-            accelOut = transform.right * acceleration;
-            Debug.DrawLine(transform.position, hitInfoLeft.point, Color.red) ;
-        } else
-        {
-            Debug.DrawRay(transform.position, whiskerLeft.direction * lookaheadDistance, Color.yellow);
-        }
-
-        bool didHitRight = Physics.Raycast(whiskerRight, out hitInfoRight, lookaheadDistance);
-        if (didHitRight)
-        {
-            //Turn left
-            accelOut = -transform.right * acceleration;
-            Debug.DrawLine(transform.position, hitInfoRight.point, Color.red);
-        }
-        else
-        {
-            Debug.DrawRay(transform.position, whiskerRight.direction * lookaheadDistance, Color.yellow);
-        }
-
-        bool didHitUp = Physics.Raycast(whiskerLeft, out hitInfoUp, lookaheadDistance);
-        if (didHitLeft)
-        {
-            //Turn down
-            accelOut = -transform.up * acceleration;
-            Debug.DrawLine(transform.position, hitInfoUp.point, Color.red);
-        }
-        else
-        {
-            Debug.DrawRay(transform.position, whiskerUp.direction * lookaheadDistance, Color.yellow);
-        }
-
-        bool didHitDown = Physics.Raycast(whiskerLeft, out hitInfoDown, lookaheadDistance);
-        if (didHitLeft)
-        {
-            //Turn up
-            accelOut = transform.up * acceleration;
-            Debug.DrawLine(transform.position, hitInfoDown.point, Color.red);
-        }
-        else
-        {
-            Debug.DrawRay(transform.position, whiskerDown.direction * lookaheadDistance, Color.yellow);
-        }
-
-        if(didHitLeft && didHitRight && didHitUp && didHitDown)
-        {
-            //Turn around!
-            accelOut = -transform.forward * acceleration;
-
-            Debug.DrawLine(transform.position, transform.forward, Color.red);
-        }
-
-        return accelOut;
-    }
-
     private void FixedUpdate()
     {
         float speed = rigidBody.linearVelocity.magnitude;
 
-        if (speed > speedMax) 
+        if (speed > speedMax)
         {
             //Enforce a top speed
             rigidBody.linearVelocity = rigidBody.linearVelocity * speedMax / speed;
         }
 
         //Orient toward velocity... but not if it is close to zero.
-        if(rigidBody.linearVelocity.sqrMagnitude > 0.01f)
+        if (rigidBody.linearVelocity.sqrMagnitude > 0.01f)
         {
             transform.forward = Vector3.Lerp(transform.forward, rigidBody.linearVelocity, 0.7f);
         }
+    }
+
+    public Vector3 Seek(Vector3 target, float acceleration)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public Vector3 Pursue(Vector3 target, float acceleration, float desiredSpeed)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public Vector3 Arrive(Vector3 target, float acceleration, float arriveInnerRadius, float arriveOuterRadius)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public Vector3 ObstacleAvoidance(float lookaheadDistance, float acceleration)
+    {
+        throw new System.NotImplementedException();
     }
 }
--- Assets/BoidSimulation.cs
+++ Assets/BoidSimulation.cs
@@ -53,11 +53,6 @@ public class BoidSimulationControl : MonoBehaviour
         }
     }
 
-    public void SetControlMode(int mode)
-    {
-        controlMode = (ControlMode)mode;
-    }
-
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Alpha1))
@@ -95,6 +90,11 @@ public class BoidSimulationControl : MonoBehaviour
         }
     }
 
+    public void SetControlMode(int mode)
+    {
+        controlMode = (ControlMode)mode;
+    }
+
     void ResetSim()
     {
         SceneManager.LoadScene(0);
@@ -107,16 +107,6 @@ public class BoidSimulationControl : MonoBehaviour
 
     private void FixedUpdate()
     {
-        //reset acceleration vectors to zero
-        for (int i = 0; i < boids.Count; i++) // For each boid...
-        {
-            boids[i].currentLinearAcceleration = Vector3.zero;
-        }
-
-        ObstacleAvoidanceBehaviour();
-
-        FoodArrivalBehaviour();
-
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hitInfo; // Will store information about the intersection if any
         bool didHit = Physics.Raycast(ray, out hitInfo, 100); // Raycast will return false if it misses, true if it hits,
@@ -135,36 +125,6 @@ public class BoidSimulationControl : MonoBehaviour
             }
         
         }
-
-        switch (controlMode)
-        {
-            case ControlMode.Seek:
-                {
-                    SeekModeControl();
-                    break;
-                }
-
-            case ControlMode.Pursue:
-                {
-                    PursueModeControl();
-                    break;
-                }
-        }
-
-        //If not doing anything, let the boids wander
-        if (!Input.GetMouseButton(0) && !Input.GetMouseButton(1))
-        {
-            WanderBehaviour();
-        }
-
-        
-
-        //enforce acceleration limits and apply velocity change
-        for (int i = 0; i < boids.Count; i++) // For each boid...
-        {
-            boids[i].currentLinearAcceleration = Vector3.ClampMagnitude(boids[i].currentLinearAcceleration, boids[i].accelMax);
-            boids[i].rigidBody.linearVelocity += boids[i].currentLinearAcceleration * Time.fixedDeltaTime;
-        }
     }
 
     private void SpawnFood()
@@ -172,99 +132,4 @@ public class BoidSimulationControl : MonoBehaviour
         Instantiate(foodPrefab, targetObject.transform.position, Random.rotation); //Instantiate a copy of a prefab for food
     }
 
-    public void ObstacleAvoidanceBehaviour()
-    {
-        for (int i = 0; i < boids.Count; i++) // For each boid...
-        {
-            Vector3 accel = boids[i].ObstacleAvoidance(obstacleAvoidanceMinRange + obstacleAvoidanceSpeedScale * boids[i].rigidBody.linearVelocity.magnitude, boids[i].accelMax);
-            boids[i].currentLinearAcceleration += accel;
-          //  Debug.DrawRay(boids[i].transform.position, accel, Color.green); // Draw acceleration
-        }
-    }
-
-    public void WanderBehaviour()
-    {
-        for (int i = 0; i < boids.Count; i++) // For each boid...
-        {
-            Vector3 accel = boids[i].Wander(0.005f, 0.02f, boids[i].accelMax * 0.2f);
-            boids[i].currentLinearAcceleration += accel;
-           // Debug.DrawRay(boids[i].transform.position, accel, Color.green); // Draw acceleration
-        }
-    }
-
-    public void FoodArrivalBehaviour()
-    {
-        //Have all fish seek food
-        for (int i = 0; i < boids.Count; i++) // For each boid...
-        {
-            float foodSeekRadius = 0.95f;
-            Collider[] colliders = Physics.OverlapSphere(boids[i].transform.position, foodSeekRadius);
-
-            Food closestFood = null;
-            float closestFoodDistance = float.PositiveInfinity;
-            foreach (Collider collider in colliders) // for each collider in this radius...
-            {
-                Food food = collider.GetComponent<Food>(); // Check if it has a Food component
-                if (food != null) // If it has this component, it will not be null. If not null, we can seek the food.
-                {
-                    float distanceToFood = Vector3.Distance(food.transform.position, boids[i].transform.position);
-                    if(distanceToFood < closestFoodDistance)
-                    {
-                        closestFoodDistance = distanceToFood;
-                        closestFood = food;
-                    }
-                }
-            }
-            
-            //If there is a food nearby
-            if(closestFood != null)
-            {
-                Vector3 accel = boids[i].Arrive(closestFood.transform.position, boids[i].accelMax, 0.03f, 0.3f);
-                boids[i].currentLinearAcceleration += accel;
-                Debug.DrawRay(boids[i].transform.position, accel, Color.green); // Draw acceleration
-            }
-        }
-    }
-
-    private void SeekModeControl()
-    {
-        for (int i = 0; i < boids.Count; i++) // For each boid...
-        {
-            //Call the Seek function on each boid to calculate an acceleration vector
-            if (Input.GetMouseButton(0))
-            {
-                Vector3 accel = boids[i].Seek(targetObject.transform.position, boids[i].accelMax);
-                boids[i].currentLinearAcceleration += accel;
-                Debug.DrawRay(boids[i].transform.position, accel, Color.green); // Draw acceleration
-            }
-            else if (Input.GetMouseButton(1))
-            {
-                Vector3 accel = boids[i].Seek(targetObject.transform.position, boids[i].accelMax);
-                boids[i].currentLinearAcceleration -= accel;
-                Debug.DrawRay(boids[i].transform.position, accel, Color.green); // Draw acceleration
-            }
-        }
-    }
-
-
-    private void PursueModeControl()
-    {
-        for (int i = 0; i < boids.Count; i++) // For each boid...
-        {
-            //Call the Pursue function on each boid to calculate an acceleration vector
-
-            if (Input.GetMouseButton(0))
-            {
-                Vector3 accel = boids[i].Pursue(targetObject.transform.position, boids[i].accelMax, boids[i].speedMax);
-                boids[i].rigidBody.linearVelocity += accel * Time.fixedDeltaTime; // Apply acceleration
-                Debug.DrawRay(boids[i].transform.position, accel, Color.green); // Draw acceleration
-            }
-            else if (Input.GetMouseButton(1))
-            {
-                Vector3 accel = boids[i].Pursue(targetObject.transform.position, boids[i].accelMax, boids[i].speedMax);
-                boids[i].rigidBody.linearVelocity -= accel * Time.fixedDeltaTime; // Apply acceleration
-                Debug.DrawRay(boids[i].transform.position, accel, Color.green); // Draw acceleration
-            }
-        }
-    }
 }
